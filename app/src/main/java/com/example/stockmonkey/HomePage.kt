323cc4112b74cc3f66823312b9b40d4fe182bafc
Alyosha-ctrl <alexeibere@gmail.com>
2026-08-31 17:16:28 -0700

    Main pieces added although obviously modifications are required

--- app/src/main/java/com/example/stockmonkey/HomePage.kt
+++ app/src/main/java/com/example/stockmonkey/HomePage.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.stockmonkey.ui.theme.StockMonkeyTheme
 
 class HomePage : ComponentActivity() {
@@ -19,8 +21,16 @@ class HomePage : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StockMonkeyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxWidth()) { innerPadding ->
                     TitleCard(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    StyleBar(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    StockList(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -34,7 +44,23 @@ class HomePage : ComponentActivity() {
 fun TitleCard(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Home Page",
-        modifier = modifier
+        modifier = modifier.padding(horizontal = 150.dp, vertical = 50.dp)
+    )
+}
+
+@Composable
+fun StyleBar(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "------------",
+        modifier = modifier.padding(horizontal = 150.dp, vertical = 25.dp)
+    )
+}
+
+@Composable
+fun StockList(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Start List",
+        modifier = modifier.padding(horizontal = 150.dp)
     )
 }
 
