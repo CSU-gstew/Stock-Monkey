127a8ab8d7e4893fd662cfc9d3e4ec997e4b3ea3
annabelleb20 <annabelle.baltes@gmail.com>
2026-09-03 23:01:29 -0700

    yayy all my database tests work

--- app/src/androidTest/java/com/example/stockmonkey/UserDatabaseTest.kt
+++ app/src/androidTest/java/com/example/stockmonkey/UserDatabaseTest.kt
@@ -5,6 +5,7 @@ import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import androidx.test.platform.app.InstrumentationRegistry
 import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
@@ -17,7 +18,8 @@ class UserDatabaseTest {
 
     @Before
     fun setup(){
-        db = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().targetContext,UserDatabase::class.java).setDriver(BundledSQLiteDriver()).build()
+        db = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().targetContext,
+            UserDatabase::class.java).setDriver(BundledSQLiteDriver()).build()
         dao = db.userDao()
     }
 
@@ -26,20 +28,97 @@ class UserDatabaseTest {
         db.close()
     }
 
+    /*
+    Tests to make sure u can add at least one user
+     */
     @Test
     fun insertTest() = runTest {
         val name = "userTest"
-        val pass = "passTest"
-        val ticker = listOf(StockTicker("tick", "Apple", 1.01F))
-        val user = UserItem(0, name, pass, ticker)
+        val user = UserItem(1, name, "passTest", listOf(StockTicker("tick", "Apple", 1.01F)))
 
-        dao.insertAll(user)
-        val datauser = dao.getUserWName(name)
-        val newname = datauser.username
-        val newpass = datauser.password
-        val newticker = datauser.tickerList
-        assertTrue(name.equals(newname))
-        assertTrue(pass.equals(newpass))
-        assertTrue(ticker.equals(newticker))
+        val uID = dao.insertAllReturnID(user)
+        val datauser = dao.getUserWID(uID[0].toInt())
+        assertTrue(user.equals(datauser))
     }
+
+    /*
+    Just testing insertall and getall works
+     */
+    @Test
+    fun getAllTest() = runTest {
+        val user1 = UserItem(0,"name1", "passTest1", listOf(StockTicker("tick", "Apple", 1.01F)))
+        val user2 = UserItem(0,"name2", "passTest2", listOf(StockTicker("tick", "Apple", 1.01F)))
+        val user3 = UserItem(0,"name3", "passTest3", listOf(StockTicker("tick", "Apple", 1.01F)))
+        dao.insertAll(user1, user2, user3)
+
+        val all = dao.getAllUsers()
+        assertTrue(all[0].username == "name1")
+        assertTrue(all[1].username == "name2")
+        assertTrue(all[2].username == "name3")
+    }
+
+    /*
+    Tests to make sure u can delete a user
+     */
+    @Test
+    fun deleteTest() = runTest{
+        val name = "userTest2"
+        val user = UserItem(1,name, "passTest", listOf(StockTicker("tick", "Apple", 1.01F)))
+        val uID = dao.insertAllReturnID(user)
+
+        val all1 = dao.getAllUsers()
+        assertTrue(all1.contains(user))
+
+        dao.delete(dao.getUserWID(uID[0].toInt()))
+
+        val all = dao.getAllUsers()
+        assertFalse(all.contains(user))
+    }
+
+    /*
+    Tests how this thing handles multiple users
+     */
+    @Test
+    fun multiTest() = runTest{
+        // ID MUST be 0, if it's 1+ it won't try to autoincrement and just stays at that ID#
+        // there's prolly a way to fix that (but the best solution is just use a 0 lol)
+        val name = "userTest3"
+        val user1 = UserItem(0,name, "passTest", listOf(StockTicker("tick", "Apple", 1.01F)))
+        val name2 = "userTest4"
+        val user2 = UserItem(0,name2, "passTest", listOf(StockTicker("tick", "Apple", 1.01F)))
+
+        val uID = dao.insertAllReturnID(user1, user2)
+        val datauser1 = dao.getUserWID(uID[0].toInt())
+        val datauser2 = dao.getUserWID(uID[1].toInt())
+        assertTrue(datauser1.id == 1)
+        assertTrue(datauser2.id == 2)
+
+        // Also checks to make sure if u add something later on it just goes to the next id, also tests duplicate usernames
+        val lateruser = UserItem(0,name, "passTest", listOf(StockTicker("tick", "Apple", 1.01F)))
+        val newID = dao.insertAllReturnID(lateruser)
+        val datauser3 = dao.getUserWID(newID[0].toInt())
+        assertTrue(datauser3.username == "userTest3")
+        assertTrue(datauser3.id == 3)
+    }
+
+    /*
+    Tests update
+     */
+    @Test
+    fun updateTest() = runTest{
+        val name = "userTest6"
+        val user = UserItem(0,name, "passTest", listOf(StockTicker("tick", "Apple", 1.01F)))
+        val GenId = dao.insertAllReturnID(user)
+
+        val newuser = user.copy(id = GenId[0].toInt(), username = "updatedUserTest6", password = "1234lol")
+        dao.update(newuser)
+
+        val all = dao.getAllUsers()
+        assertTrue(all.size == 1)
+        val testuser = dao.getUserWID(GenId[0].toInt())
+        assertTrue(testuser.password == "1234lol")
+        assertTrue(testuser.username == "updatedUserTest6")
+    }
+
+
 }
--- app/src/main/java/com/example/stockmonkey/UserDao.kt
+++ app/src/main/java/com/example/stockmonkey/UserDao.kt
@@ -4,22 +4,40 @@ import androidx.room3.Dao
 import androidx.room3.Delete
 import androidx.room3.Insert
 import androidx.room3.Query
-import androidx.room3.Transaction
 import androidx.room3.Update
 
+/*
+Methods for the User database
+
+Maybe playing it a little too safe with only being able to access via ID
+But I can always add more methods
+ */
 @Dao
 interface UserDao {
+    // General insert
     @Insert
     suspend fun insertAll(vararg user: UserItem)
 
+    // If u need to insert and need the ID and theoretically don't know it
+    // (mostly for the update function)
+    @Insert
+    suspend fun insertAllReturnID(vararg user: UserItem): List<Long>
+
+    // Deletes it idk, just make sure you're referring to the right user (via ID is best)
     @Delete
     suspend fun delete(user: UserItem)
 
+    // Updates it, best example of usage is in the test file
     @Update
     suspend fun update(user: UserItem)
 
+    // Lets u select a user with an ID
     @Query(
-        """SELECT * FROM user_item_table WHERE username LIKE :name LIMIT 1"""
+        """SELECT * FROM user_item_table WHERE id LIKE :id LIMIT 1"""
     )
-    suspend fun getUserWName(name: String): UserItem
+    suspend fun getUserWID(id: Int): UserItem
+
+    // Returns all users as a list
+    @Query("""SELECT * FROM user_item_table""")
+    suspend fun getAllUsers(): List<UserItem>
 }
