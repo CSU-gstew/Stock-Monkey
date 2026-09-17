056d37c9c0a6ef930cc4c04e93e6331ec4515831
annabelle-baltes <annabelle.baltes@gmail.com>
2026-09-17 13:58:55 -0700

    Going to continue working on UI at home

--- app/src/main/java/com/example/stockmonkey/LoginActivity.kt
+++ app/src/main/java/com/example/stockmonkey/LoginActivity.kt
@@ -208,7 +208,7 @@ fun LoginTitleCard() {
         modifier = Modifier
             .padding(bottom = 16.dp)
             .fillMaxWidth(),
-        color = Color(0xFF3C2253),
+        color = MaterialTheme.colorScheme.primary,
         textAlign = TextAlign.Center,
     )
     Text(
@@ -235,7 +235,7 @@ fun UsernameInput(
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(text = "Username", fontFamily = FontFamily.Monospace, color = Color(0xFF7F6699)) },
+        label = { Text(text = "Username", fontFamily = FontFamily.Monospace, color = MaterialTheme.colorScheme.secondary) },
         singleLine = true,
         isError = errorMessage != null,
         supportingText = {
@@ -265,7 +265,7 @@ fun PasswordInput(
     OutlinedTextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(text = "Password", fontFamily = FontFamily.Monospace, color = Color(0xFF7F6699)) },
+        label = { Text(text = "Password", fontFamily = FontFamily.Monospace, color = MaterialTheme.colorScheme.secondary) },
         singleLine = true,
         isError = errorMessage != null,
         supportingText = {
@@ -277,7 +277,7 @@ fun PasswordInput(
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
         trailingIcon = {
             TextButton(onClick = onToggleVisibility) {
-                Text(text = if (passwordVisible) "Hide" else "Show", fontFamily = FontFamily.Monospace, color = Color(0xFF7F6699))
+                Text(text = if (passwordVisible) "Hide" else "Show", fontFamily = FontFamily.Monospace, color = MaterialTheme.colorScheme.secondary)
             }
         },
         modifier = modifier
@@ -319,9 +319,10 @@ fun SignUpNavButton(
     TextButton(
         onClick = onClick,
         modifier = modifier
+            .padding(bottom = 30.dp)
     ) {
 
-        Text(text = "Create Account", fontSize = 14.sp, fontFamily = FontFamily.Monospace)
+        Text(text = "Create Account", fontSize = 14.sp, fontFamily = FontFamily.Monospace, color = MaterialTheme.colorScheme.primary)
     }
 }
 
--- app/src/main/java/com/example/stockmonkey/ui/theme/Theme.kt
+++ app/src/main/java/com/example/stockmonkey/ui/theme/Theme.kt
@@ -1,7 +1,5 @@
 package com.example.stockmonkey.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -20,7 +18,7 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -37,11 +35,11 @@ private val LightColorScheme = lightColorScheme(
 fun StockMonkeyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
